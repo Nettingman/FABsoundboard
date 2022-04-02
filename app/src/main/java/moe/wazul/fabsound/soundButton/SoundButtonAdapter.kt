b969ea2cbddb9637e23cf3ebfff9fe2ad1638d8e
Botond Hende <nettingman@gmail.com>
2022-04-02 20:35:31 +0200

    wip: dynamically add more soundbuttons

---
+++ app/src/main/java/moe/wazul/fabsound/soundButton/SoundButtonAdapter.kt
@@ -0,0 +1,28 @@
+package moe.wazul.fabsound.soundButton
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import moe.wazul.fabsound.R
+import moe.wazul.fabsound.databinding.SoundButtonBinding
+
+class SoundButtonAdapter(private val soundButtonDatas: MutableList<SoundButtonData>) : RecyclerView.Adapter<SoundButtonAdapter.SoundButtonViewHolder>()
+{
+    class SoundButtonViewHolder(val binding : SoundButtonBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = SoundButtonViewHolder (
+        SoundButtonBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    )
+
+    override fun onBindViewHolder(holder: SoundButtonViewHolder, position: Int) {
+        val soundButtonData = soundButtonDatas[position]
+        holder.binding.apply {
+            btnSound.text = soundButtonData.name
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return soundButtonDatas.size
+    }
+}
---
+++ app/src/main/java/moe/wazul/fabsound/soundButton/SoundButtonData.kt
@@ -0,0 +1,5 @@
+package moe.wazul.fabsound.soundButton
+
+data class SoundButtonData(var name : String) {
+
+}
